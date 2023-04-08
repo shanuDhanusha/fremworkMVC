937c50dfc9d22d42414c93144e322d3d6c19fdfe
shanuDhanusha <2020cs186@stu.ucsc.cmb.ac.lk>
2023-04-08 06:05:56 -1200

    forme dising

--- app/views/pages/additem.php
+++ app/views/pages/additem.php
@@ -7,29 +7,6 @@
                    
                   <div class="item-contener-1">
 
-                        <div class="input-filed-left">
-                              <div class="input-filed-div">
-                                   <input type="text" name="category" id="category" >
-                                   <span><?php echo "Enter category";?></span>
-
-                                 </div>
-
-                                 <div class="input-filed-div">
-                                   <input type="text" name="category" id="category" >
-                                   <span><?php echo "Enter category";?></span>
-
-                                 </div>
-
-                                 <div class="input-filed-div">
-                                   <input type="text" name="category" id="category" >
-                                   <span><?php echo "Enter category";?></span>
-
-                                 </div>
-
-                         </div>
-                       <div class="barial-div"></div>
-                       <div class="input-filed-right"></div>
-                     
 
 
 
